a988f2b8c6476909b5cd585f699de970d6c8c7fc
Lennard Gerdes <lennard.gerdes@gmx.net>
2020-10-06 14:54:15 +0000

    Counter in For-Loop

---
+++ Normal way/counterInForLoop.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'banana', 'grapes', 'pear']
+
+counter = 0
+for value in my_list:
+    print counter, value
+    counter += 1
+    
+# Result 
+# 0 apple
+# 1 banana
+# 2 grapes
+# 3 pear 
---
+++ Pythonic way/counterInForLoop.py
@@ -0,0 +1,10 @@
+my_list = ['apple', 'banana', 'grapes', 'pear']
+
+for counter, value in enumerate(my_list):
+    print counter, value
+    
+# Result 
+# 0 apple
+# 1 banana
+# 2 grapes
+# 3 pear 
