5c21a708ddfb92f491a92def724635ea40d72dd0
Preetesh Verma <verma.preetesh21@gmail.com>
2020-10-10 18:53:33 +0530

    list of tuples to list of list

--- Pythonic way/transposeMatrix.py
+++ Pythonic way/transposeMatrix.py
@@ -2,7 +2,11 @@ x = [[31,17],
 [40 ,51],
 [13 ,12]]
     
-print(list(zip(*x)))
+t=list(zip(*x))
+# convert list of tuples to list of list 
+res = [list(ele) for ele in t]
+print(res)
+
 
 # Result
-# [(31, 40, 13), (17, 51, 12)]
+# [[31, 40, 13], [17, 51, 12]]
