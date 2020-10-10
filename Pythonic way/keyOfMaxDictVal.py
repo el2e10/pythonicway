88962889af82e6866d2fc9fb7900a04ad75a6033
Alisa Khieu <47487416+anqkhieu@users.noreply.github.com>
2020-10-10 13:17:42 -0700

    Create keyOfMaxDictVal.py

---
+++ Pythonic way/keyOfMaxDictVal.py
@@ -0,0 +1,4 @@
+sample_dict = {'Turtles':5, 'Dogs':3, 'Cats':3, 'Snakes':2}
+max_key = max(sample_dict, key=sample_dict.get)
+
+print(f"In the dictionary, there are the most {max_key} ({sample_dict[max_key]}).")
